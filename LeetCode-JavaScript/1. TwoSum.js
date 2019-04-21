4121db056dffa83b97e6cd4c3d126dc7b19e1f64
Cliff Su <stu01509@yahoo.com.tw>
2019-04-21 21:39:34 +0800

    Add 1. TwoSum.js

---
+++ LeetCode-JavaScript/1. TwoSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    let numsMap = new Map();
+    let result = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        const subNum = target - nums[i];
+        if (numsMap.has(subNum) && numsMap.get(subNum) !== i) {
+            result[0] = numsMap.get(subNum);
+            result[1] = i;
+            return result;
+        } else {
+            numsMap.set(nums[i], i);
+        }
+    }
+
+}
