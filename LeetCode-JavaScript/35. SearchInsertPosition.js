9e21d7af332aac81b7507c508c38dd11ee459839
Cliff Su <stu01509@yahoo.com.tw>
2019-08-29 20:41:35 +0800

    Add 35. SearchInsertPosition.js

---
+++ LeetCode-JavaScript/35. SearchInsertPosition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (target <= nums[i]) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+
+console.log(searchInsert([1,3,5,6], 5));
+console.log(searchInsert([1,3,5,6], 2));
+console.log(searchInsert([1,3,5,6], 7));
+console.log(searchInsert([1,3,5,6], 0));
