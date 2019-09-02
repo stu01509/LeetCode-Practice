b84ab603bd100d5711a03a28827a6d928b0f17f9
Cliff Su <stu01509@yahoo.com.tw>
2019-09-02 22:30:19 +0800

    Add  26. RemoveDuplicatesfromSortedArray.js

---
+++ LeetCode-JavaScript/26. RemoveDuplicatesfromSortedArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+    if (nums.length === 0 || nums === null) {
+        return 0;
+    }
+    let count = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] !== nums[i]) {
+            nums[count++] = nums[i];
+        }
+    }    
+    return count;
+};
+
+console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
