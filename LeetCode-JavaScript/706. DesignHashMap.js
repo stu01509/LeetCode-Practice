69dcd6960a546ac609c76706d2ab46693f179581
Cliff Su <stu01509@yahoo.com.tw>
2019-11-02 10:30:06 +0800

    Add 706. DesignHashMap.js

---
+++ LeetCode-JavaScript/706. DesignHashMap.js
@@ -0,0 +1,46 @@
+/**
+ * Initialize your data structure here.
+ */
+const MyHashMap = function() {
+    this.data = {};
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+    this.data[key] = value;
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    if (this.data[key] === undefined) {
+        return -1;
+    } else {
+        return this.data[key];
+    }
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+    delete this.data[key];
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
