8e1e49a2d36e5117e4b22242038c417232d01cdf
Cliff Su <stu01509@yahoo.com.tw>
2019-11-29 21:38:40 +0800

    Add 80. RemoveDuplicatesfromSortedArrayII.js

--- LeetCode-JavaScript/80. RemoveDuplicatesfromSortedArrayII.js
+++ LeetCode-JavaScript/80. RemoveDuplicatesfromSortedArrayII.js
@@ -2,29 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-const removeDuplicates = function(nums) {
-    if (nums.length === 0 || nums === null ) {
+const removeDuplicates = function (nums) {
+    if (nums.length === 0 || nums === null) {
         return 0;
     }
-
     let index = 0;
-    let times = 0;
-    for (let i = 2; i < nums.length; i++) {
-        if (times === 2) {
-            console.log(1);
-            nums[index] = nums[i];
-            times = 0;
-        } else {
-            if (nums[i - 1] === nums[i] && nums[i - 1] !== nums[i - 2]) {
-                times++;
-                index++;          
-            } else if (nums[i - 1] !== nums[i] && nums[i - 1] !== nums[i - 2]) {
-                nums[++index] = nums[i];                
-            }
+    for (let i = 0; i < nums.length; i++) {
+        if (i < 2 || nums[i] > nums[index - 2]) {
+            nums[index++] = nums[i];
         }
     }
-    console.log(nums);    
+    return index;
 };
 
-// console.log(removeDuplicates([1,1,1,2,2,3]));
-console.log(removeDuplicates([0,0,1,1,1,1,2,3,3]));
+console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]));
