9965412ec21000b01a5dbaea28b69878b7ac9bda
Cliff Su <stu01509@yahoo.com.tw>
2022-07-03 18:26:39 +0800

    feat: add Number of Islands

---
+++ LeetCode-JavaScript/200. NumberofIslands.js
@@ -0,0 +1,43 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+const numIslands = function (grid) {
+    let result = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === '1') {
+                result++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+
+    return result;
+};
+
+const dfs = function (grid, i, j) {
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === '0') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs(grid, i + 1, j);
+    dfs(grid, i - 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
+};
+
+console.log(numIslands([
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]));
+
+console.log(numIslands([
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
+]));
