41a0866a56790b0395059fef5eca24186bff3474
Cliff Su <stu01509@yahoo.com.tw>
2022-07-03 18:13:45 +0800

    feat: add 376. Wiggle Subsequence

---
+++ LeetCode-JavaScript/376. WiggleSubsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const wiggleMaxLength = function (nums) {
+    if (nums.length < 2) {
+        return nums.length;
+    }
+
+    let up = 1;
+    let down = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            up = down + 1;
+        } else if (nums[i] < nums[i - 1]) {
+            down = up + 1;
+        }
+    }
+    return Math.max(up, down);
+};
+
+console.log(wiggleMaxLength([1, 7, 4, 9, 2, 5]));
+console.log(wiggleMaxLength([1, 17, 5, 10, 13, 15, 10, 5, 16, 8]));
+console.log(wiggleMaxLength([1, 2, 3, 4, 5, 6, 7, 8, 9]));
