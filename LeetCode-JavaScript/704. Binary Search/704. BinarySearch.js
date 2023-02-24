86ffc95c563da800cf2817dd27def974772095a8
Cliff Su <stu01509@yahoo.com.tw>
2023-02-24 21:43:56 +0800

    feat: add 704. Binary Search

---
+++ LeetCode-JavaScript/704. Binary Search/704. BinarySearch.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) {
+      return middle;
+    }
+
+    if (nums[middle] > target) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
