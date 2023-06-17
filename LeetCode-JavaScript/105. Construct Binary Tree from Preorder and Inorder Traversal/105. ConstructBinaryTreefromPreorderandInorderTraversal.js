f562832daf89be4e019f4bda53ffe1a3f5a7a32c
Cliff Su <stu01509@yahoo.com.tw>
2023-06-17 23:13:24 +0800

    feat: add 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ LeetCode-JavaScript/105. Construct Binary Tree from Preorder and Inorder Traversal/105. ConstructBinaryTreefromPreorderandInorderTraversal.js
@@ -0,0 +1,33 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+};
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+const buildTree = function (preorder, inorder) {
+    if (!preorder.length || !inorder.length) {
+        return null;
+    }
+
+    const root = new TreeNode(preorder[0]);
+    const mid = inorder.indexOf(preorder[0]);
+
+    root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+    root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+    return root;
+};
+
+console.log(buildTree([3, 9, 20, 15, 7], [9, 3, 15, 20, 7]));
+console.log(buildTree([-1], [-1]));
