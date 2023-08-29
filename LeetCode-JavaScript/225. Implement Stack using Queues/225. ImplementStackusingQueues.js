bf77a8c4a7421474be135b99b5799b4d1700555a
Cliff Su <stu01509@yahoo.com.tw>
2023-08-29 21:28:06 +0800

    feat: add 225. Implement Stack using Queues

---
+++ LeetCode-JavaScript/225. Implement Stack using Queues/225. ImplementStackusingQueues.js
@@ -0,0 +1,50 @@
+class MyStack {
+    constructor() {
+        this.queue = [];
+    }
+
+    /** 
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.queue = [x, ...this.queue]
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        return this.queue.shift();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.queue[0];
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        return this.queue.length === 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+const myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.top());
+console.log(myStack.pop());
+console.log(myStack.empty());
